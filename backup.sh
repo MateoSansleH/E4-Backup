c68daec852071462a2e32d36e0cfd6c71e18c52a
MateoSansleH <73388681+MateoSansleH@users.noreply.github.com>
2021-03-29 19:56:25 +0200

    amelioration backup DC1

--- backup.sh
+++ backup.sh
@@ -11,25 +11,47 @@
 
 #Backup DC1
 #l'utilisateur AD backup_user est le seul avec l'administrateur a avoir acces au partage "Sauvegarde"
-LDAP_USER="backup_user"
-LDAP_PASSWORD="Not24get"
-LDAP_DOMAIN="megaprod.lan"
+DC1_LDAP_USER="backup_user"
+DC1_LDAP_PASSWORD="Not24get"
+DC1_LDAP_DOMAIN="megaprod.lan"
+DC1_LDAP_PARTAGE="//10.152.53.2/sauvegardes"
+DC1_LDAP_FOLDER_BACKUP="WindowsImageBackup"
 
 
 DATESAVE=$(date "+%Y%m%d")
 
 
 
+architecture(){
+    ##
+    ##  Création de l'arborecence + assurence dossier toujours present.
+    ##
+    mkdir -p /home/backup_user/temp # Construction des Backup
+    rm -rf /home/backup_user/temp/* # Assurance que Temp est vide
+
+    mkdir -p /home/backup_user/backup # Repertoire contenant les backups 
+    mkdir -p /home/backup_user/restore # Repertoire contenant les fichier lors d'une restoration
+    mkdir -p /home/backup_user/script # Repertoire contenant les scripts
+}
+
 creation_backupfolder(){
-    mkdir -p /home/backup_user/backup-megaprod-${DATESAVE}
+    ##
+    ##  Création du repertoire contenant la sauvegarde du jour. 
+    ##          - forme backup-megaprod-20210329
+    ##
+    mkdir -p /home/backup_user/Backup/backup-megaprod-${DATESAVE}
 }
 
 backup_DC1(){
+    ##
+    ##  Montage du paratage conetant la sauvegarde 
+    ##  Recuperation de la sauvegarde pour traitement
+    ##  Demontage / fermeture du partage du partage
+    ##
     mkdir -p /media/partage
-    mkdir -p /home/backup_user/backup-megaprod-${DATESAVE}/DC1
-    mount -t cifs //10.152.53.2/sauvegardes /media/partage -o username=administrateur,workgroup=megaprod.lan,password=Not24get
-    mv /media/partage/WindowsImageBackup /home/backup_user/backup-megaprod-${DATESAVE}/DC1
-    
+    mkdir -p /home/backup_user/temp/DC1
+    mount -t cifs ${DC1_LDAP_PARTAGE} /media/partage -o username=${DC1_LDAP_USER},workgroup=${DC1_LDAP_DOMAIN},password=${DC1_LDAP_DOMAIN}
+    mv /media/partage/${DC1_LDAP_FOLDER_BACKUP} /home/backup_user/temp/DC1
     umount /media/partage
 }
 
@@ -41,9 +63,10 @@ compression_DC1(){
 
 
 main(){
+    architecture
     creation_backupfolder
     backup_DC1
-    compression_DC1
+    #compression_DC1
 }
 
 main
